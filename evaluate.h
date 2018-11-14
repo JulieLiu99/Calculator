7bc327ce982299ec31c882d7b403342f5c12dcb6
Julie Liu <41465280+JulieLiu99@users.noreply.github.com>
2018-11-15 01:34:48 +0400

    Add files via upload

---
+++ evaluate.h
@@ -0,0 +1,29 @@
+#ifndef EVALUATE_H
+#define EVALUATE_H
+
+#include<iostream>
+#include <fstream>
+
+using namespace std;
+
+
+class E {
+	public:
+        bool areParanthesisBalanced(string expr);
+        string lineOperation(string afterEquals);
+        bool isVariablePresent(string value);
+        bool isOperatorPresent(string value);
+        string ReplaceString(string subject, const string& search, const string& replace);
+        bool isoperator(string str);
+        void mapEvaluation(string filename, string k);
+        string evaluate(vector<string>  &y);
+        float eval(float x1, float x2, string sign);
+        vector<string> tokenize(string x);
+        int convertOpToInt (string str);
+        bool isleq(string opA, string opB);
+        vector<string> infix2postfix(vector<string> &x);
+        bool isoperator(char ch);
+        void writeToFile(string line, string filename);
+};
+
+#endif
---
+++ mycalc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "evaluate.h"
+
+using namespace std;
+
+int main(int count, char * args[]) {
+
+    E e;
+    string inputFile = "";
+    string outputFile = "";
+    
+    //take as parameters names of the input and output textfiles
+    for (int i = 1; i < count; i++) {
+        if ((string(args[i]) == "-i") && i + 1 < count)
+            inputFile = args[i + 1];
+        else if ((string(args[i]) == "-o") && i + 1 < count)
+            outputFile = args[i + 1];
+    }
+    //remind user of the way to invocate the application
+    if (inputFile == "" || outputFile == "") {
+        cout << "Usage ./mycalc -i <inputfile> -o <outputfile>>" << endl;
+        return -1;
+    }
+    //print out names of the input and output textfiles
+    cout << "Input file is " << inputFile << endl;
+    cout << "Output file is " << outputFile << endl;
+
+    e.mapEvaluation(inputFile, outputFile);
+}
+
+
+
+
+
